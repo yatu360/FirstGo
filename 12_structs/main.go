4f832568bb66bfe6ce0e23807b01682e7d5d695a
yatu360 <yatu360@gmail.com>
2021-06-26 20:01:40 +0100

    lets go

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -16,7 +16,12 @@ type Person struct {
 
 func main(){
 
-	person1 := Person{firstName:"Samantha", lastName: ""
+	person1 := Person{firstName:"Samantha", lastName: "Smith", city:"London", age: 25, gender: "f"}
+
+	person2 := Person{"Samantha", "Smith", "London", "f", 24}
+
+	fmt.Println(person1)
+	fmt.Println(person2)
 
 
 }
